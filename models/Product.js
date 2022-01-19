02273f52b1fb0b2c5a1b6423c1fe5afcb030f747
premthanachot <72982215+premthanachot@users.noreply.github.com>
2022-01-19 22:08:46 +0700

    commit end v.2

--- models/Product.js
+++ models/Product.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    description: { type: String, required: true, unique: true },
-    img: { type: String, required: true, unique: true },
-    categories: { type: String, required: true, unique: true },
-    price: { type: String, required: true, unique: true },
-    size: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    img: { type: String, required: true },
+    categories: { type: String, required: true },
+    price: { type: Number, required: true},
+    // size: { type: String, required: true, unique: true },
   },
   { timestamps: true }
 );
